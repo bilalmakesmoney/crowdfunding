7376ba37035ee34882f5a6898ad59dae3cdcf7f4
Bilal-Bytes <mb7899011@gmail.com>
2024-12-04 19:26:39 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,18 @@
+import Background from "../public/background.png";
+import Header from "./Header";
+import Hero from "./Hero";
+
 const Home = () => {
-  return <h1 className="text-9xl text-blue-300">hello</h1>;
+  return (
+    <div className="w-screen min-h-screen px-12 ">
+      <img
+        src={Background}
+        alt=""
+        className="absolute w-full h-full -top-[0px] left-0 object-cover opacity-5"
+      />
+      <Header />
+      <Hero />
+    </div>
+  );
 };
 export default Home;
