cee9ef6ab4dbdc37162cb115491d7aa86e5cdc35
Bilal-Bytes <mb7899011@gmail.com>
2024-12-07 21:24:12 +0530

    commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,29 +3,31 @@ import image from "../../public/hero.png";
 
 const Hero = () => {
   return (
-    <div className="w-full flex flex-col lg:flex-row mt-20 relative items-center lg:items-start max-w-[1200px] mx-auto px-4">
+    <div className="w-full grid grid-cols-1 lg:grid-cols-2 place-items-center gap-8 items-center mt-20 max-w-[1400px] mx-auto px-6">
       <div className="text-center lg:text-left">
-        <h1 className="text-4xl lg:text-[50px] xl:text-[60px] leading-tight lg:leading-[60px] xl:leading-[75px] font-bold">
-          <span className="text-primary">Trust</span> shouldn’t be a{" "}
-          <br className="hidden lg:block" /> battle when{" "}
-          <span className="text-primary">life</span> is on{" "}
-          <br className="hidden lg:block" /> the{" "}
-          <span className="text-primary">line.</span>
+        <h1 className="text-4xl lg:text-[50px] xl:text-[56px] leading-tight lg:leading-[65px] xl:leading-[80px] font-bold">
+          Bee the <span className="text-primary">Hope</span>, Spread the{" "}
+          <span className="text-primary">Buzz</span>, and Save{" "}
+          <span className="text-primary">Lives</span> Together
         </h1>
-        <p className="text-white/50 mt-3 text-sm lg:text-md max-w-[500px] mx-auto lg:mx-0">
+        <p className="text-white/50 mt-4 text-sm lg:text-md xl:text-lg max-w-[600px] mx-auto lg:mx-0">
           Unite passion and support to create something extraordinary. Start
           your journey today.
         </p>
-        <div className="flex mt-5 w-fit justify-center items-center px-4 py-2 bg-primary rounded-md gap-1 cursor-pointer mx-auto lg:mx-0">
-          <span>Start Fundraising</span>
-          <RxArrowTopRight className="text-2xl" />
+        <div className="flex mt-6 w-fit justify-center items-center m:px-4 md:py-2 px-3 py-1.5 lg:px-6 lg:py-3 bg-primary rounded-md gap-2 cursor-pointer mx-auto lg:mx-0 hover:bg-primary/90 transition-all">
+          <span className="text-sm lg:text-md xl:text-lg">
+            Start Fundraising
+          </span>
+          <RxArrowTopRight className="text-2xl xl:text-3xl" />
         </div>
       </div>
-      <img
-        src={image}
-        alt="hero"
-        className="h-64 md:h-80 lg:h-[500px] mt-8 lg:mt-0 lg:absolute lg:-top-10 lg:right-20 lg:translate-x-[20%] -z-50 max-w-full"
-      />
+      <div className="flex justify-center lg:justify-end">
+        <img
+          src={image}
+          alt="hero"
+          className="h-auto max-h-[420px] max-w-full"
+        />
+      </div>
     </div>
   );
 };
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,11 +4,11 @@ import Header from "../components/Header";
 
 const LandingPage = () => {
   return (
-    <div className="w-screen min-h-screen md:px-12">
+    <div className="w-screen min-h-screen md:px-12 background relative">
       <img
         src={Background}
+        className="w-full h-full absolute top-0 left-0 opacity-5"
         alt=""
-        className="absolute w-full h-full -top-[0px] left-0 object-cover opacity-5"
       />
       <Header />
       <Hero />
